4ac7dcc367dcacd143a6369728de835be0d8cc66
pierre-yvesletraon <pyletraon>
2025-02-11 19:35:32 +0100

    add: added search by category feature, fixed small path/rendering bug w/ individual search results from catalog & allProducts pages

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import express from 'express';
 
 import mainController from "./controllers/main.controller.js";
 
-const router = Router();
+const router = express.Router();
 
 router.get("/", mainController.renderHomePage);
 router.get("/catalog", mainController.renderCatalogPage);
