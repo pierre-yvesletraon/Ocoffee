dcf8e234ab9e61bbb3610f7a4e879490cb2c440b
Pierre-Yves Le Traon <pyletraon@gmail.com>
2025-02-14 16:09:40 +0100

    modif d'index.js

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.send(`Heure actuelle de la base de données : ${result.rows[0].now}`);
-  } catch (err) {
-    console.error("Erreur lors de la requête à la base de données :", err);
-    res.status(500).send("Erreur serveur");
-  }
-});
 
 app.use(router);
 
